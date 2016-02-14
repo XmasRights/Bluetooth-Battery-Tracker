a894d8c66e4bd9c7ef20b7f79f96bbda257117ba
XmasRights <chris.fonseka@googlemail.com>
2016-02-14 16:12:41 +0000

    Initial Commit

---
+++ Bluetooth Battery Tracker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Bluetooth Battery Tracker
+//
+//  Created by Chris Home on 14/02/2016.
+//  Copyright © 2016 Christmas House. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
