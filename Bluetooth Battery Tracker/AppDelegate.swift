3ea3e205449d5ddafb06a3b1e46ae85975f5fc6c
XmasRights <chris.fonseka@googlemail.com>
2016-02-14 16:23:16 +0000

    Added assets for Menu Bar icon, and added a dummy action to make sure it actually works

--- Bluetooth Battery Tracker/AppDelegate.swift
+++ Bluetooth Battery Tracker/AppDelegate.swift
@@ -12,16 +12,26 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
 
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+    func applicationDidFinishLaunching(aNotification: NSNotification)
+    {
+        if let button = statusItem.button
+        {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = Selector("printQuote:")
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
+    
+    func printQuote(sender: AnyObject)
+    {
+        let quote = "This shizzle works, that's dope"
+        print(quote)
+    }
 
 }
 
