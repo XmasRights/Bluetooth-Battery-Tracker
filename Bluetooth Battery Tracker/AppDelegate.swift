5388b7d2ae509ab4daf67ccfc8b150520f89fc56
XmasRights <chris.fonseka@googlemail.com>
2016-02-14 16:47:25 +0000

    Menu Bar Button action now shows a blank popup window

--- Bluetooth Battery Tracker/AppDelegate.swift
+++ Bluetooth Battery Tracker/AppDelegate.swift
@@ -12,26 +12,48 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
-
+    let popover    = NSPopover()
+    
     func applicationDidFinishLaunching(aNotification: NSNotification)
     {
         if let button = statusItem.button
         {
             button.image = NSImage(named: "StatusBarButtonImage")
-            button.action = Selector("printQuote:")
+            button.action = Selector("togglePopover:")
         }
+        
+        popover.contentViewController = DeviceTableViewController(nibName: "DeviceTableViewController", bundle: nil)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
     
-    func printQuote(sender: AnyObject)
+    func showPopover(sender: AnyObject?)
     {
-        let quote = "This shizzle works, that's dope"
-        print(quote)
+        if let button = statusItem.button
+        {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MinY)
+        }
     }
-
+    
+    func closePopover(sender: AnyObject?)
+    {
+        popover.performClose(sender)
+    }
+    
+    func togglePopover (sender: AnyObject?)
+    {
+        if popover.shown
+        {
+            closePopover(sender)
+        }
+        else
+        {
+            showPopover(sender)
+        }
+    }
 }
 
---
+++ Bluetooth Battery Tracker/DeviceTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DeviceTableViewController.swift
+//  Bluetooth Battery Tracker
+//
+//  Created by Chris Home on 14/02/2016.
+//  Copyright © 2016 Christmas House. All rights reserved.
+//
+
+import Cocoa
+
+class DeviceTableViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
