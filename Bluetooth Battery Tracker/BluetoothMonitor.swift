30164adc6f100d2938b32ef143cc322eed1a0eb6
XmasRights <chris.fonseka@googlemail.com>
2016-02-16 17:09:43 +0000

    Added the Asset Folder, and BluetoothMonitor class file

---
+++ Bluetooth Battery Tracker/BluetoothMonitor.swift
@@ -0,0 +1,13 @@
+//
+//  BluetoothMonitor.swift
+//  Bluetooth Battery Tracker
+//
+//  Created by Chris Home on 14/02/2016.
+//  Copyright © 2016 Christmas House. All rights reserved.
+//
+
+import Cocoa
+
+class BluetoothMonitor: NSObject
+{
+}
